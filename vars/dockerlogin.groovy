8a0fcb817db1cf242ce2aed5bca716ca837ca575
Meghraj Fand <meghraj8788@gmail.com>
2025-12-30 18:53:05 +0530

    Update dockerlogin.groovy

--- vars/dockerlogin.groovy
+++ vars/dockerlogin.groovy
@@ -1,14 +1,14 @@
-def call(String credID, String image){
-  withCredentials([
+def call(String credID, String image) {
+    withCredentials([
         usernamePassword(
             credentialsId: credID,
             usernameVariable: 'DOCKER_USER',
             passwordVariable: 'DOCKER_PASS'
         )
     ]) {
-        sh '''
-            echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-            docker push "$DOCKER_USER/'${image}'"
-        '''
+        sh """
+            echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
+            docker push "\$DOCKER_USER/${image}"
+        """
     }
 }
