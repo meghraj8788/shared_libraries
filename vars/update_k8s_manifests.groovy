84c844b8e4da8aa4c305900be2e9adb243aa8257
Meghraj Fand <meghraj8788@gmail.com>
2026-01-22 19:11:08 +0530

    Fix git remote URL format in update_k8s_manifests

--- vars/update_k8s_manifests.groovy
+++ vars/update_k8s_manifests.groovy
@@ -47,7 +47,7 @@ def call(Map config = [:]) {
                 git commit -m "Update image tags to ${imageTag} and ensure correct domain [ci skip]"
                 
                 # Set up credentials for push
-                git remote set-url origin https://\${GIT_USERNAME}:\${GIT_PASSWORD}@https://github.com/meghraj8788/3-tier-commerce-app/.git
+                git remote set-url origin https://\${GIT_USERNAME}:\${GIT_PASSWORD}@https://github.com/meghraj8788/3-tier-commerce-app.git
                 git push origin HEAD:\${GIT_BRANCH}
             fi
         """
